2853ef6e322c27c7016677dc49a0eb3aa7261a97
Nguyen Ai Dan <dan3366881@gmail.com>
2024-07-27 01:49:23 +0700

    Your commit message

---
+++ SinhVien/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $('#tab1').addClass('active');
+  $('#t1').addClass('active, click');
+
+
+  $('.tab').click(function () {
+
+    var tabId = $(this).data('tab');
+    $('.tab').removeClass('active, click');
+    $(this).addClass('active, click');
+    $('.tab-content').removeClass('active');
+    $('#' + tabId).addClass('active');
+  });
+});
+
+
+$(document).ready(function () {
+  $('.btnSua').click(function () {
+    $('.input-active').prop('disabled', false);
+  })
+
+  $('.btnCapNhat').click(function () {
+    $('.input-active').prop('disabled', true);
+  })
+})
+showAlert()
+{
+alert("Cập Nhật thành công");
+}
